4dfd423491c7871c58f3f0b444f28888976cecb9
Mouli <venkatmouli823@gmail.com>
2021-05-29 15:45:26 +0530

    2.14

--- part2/countries/src/components/DisplayCountry.js
+++ part2/countries/src/components/DisplayCountry.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const DisplayCountry = ({country}) => {
+  const [weather,setWeather] = useState({})
+  const key = process.env.REACT_APP_API_KEY;
+  const query = country.capital;
+  useEffect(()=>{
+    axios.get(`http://api.weatherstack.com/current?access_key=${key}&query=${query}`)
+    .then((response)=>{
+      console.log(response.data);
+      setWeather(response.data);
+    })
+    .catch((e)=> console.log(e))
+  },[key,query])
+  
     return (
       <div>
             <h1>{country.name}</h1>
@@ -15,8 +28,20 @@ const DisplayCountry = ({country}) => {
                </ul>
                <img src={country.flag} width="100px"/>
              </div>
+            
+               {weather.current !== undefined ?
+               <div>
+               <h3>Weather in {country.capital}</h3>
+               <p> <strong>temperature: </strong>
+               {weather.current.temperature} Celcius</p>
+               <img src={weather.current.weather_icons[0]}/>
+               <p><strong>wind: </strong> {weather.current.wind_speed} direction {weather.current.wind_dir}</p>
+               </div>
+                :
+                <p>No Weather data for this country</p>
+              }
+             
         </div>
     )
   }
-
 export default DisplayCountry;
