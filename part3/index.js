d3538fef4c30fc9df710d6154032adb4d46f75dd
Mouli <venkatmouli823@gmail.com>
2021-06-01 13:16:53 +0530

    3.2

--- part3/index.js
+++ part3/index.js
@@ -31,6 +31,11 @@ app.get('/',(req,res)=>{
 app.get('/api/persons',(req,res)=>{
     res.json(persons);
 })
+
+app.get('/info',(req,res)=>{
+  const date = new Date();
+  res.send(`<p> Phonebook has info for ${persons.length} people</p> <p> ${date} </p>`)
+})
 const PORT = 3001
 app.listen(PORT,()=>{
     console.log(`Server is running at http://localhost:${PORT}/`);
