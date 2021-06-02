e54bb98fb5d275d8cf4aa513a6a9d42c1b972a46
Mouli <venkatmouli823@gmail.com>
2021-06-02 12:24:13 +0530

    3.7

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const morgan = require('morgan');
 const app = express();
 
 app.use(express.json())
+app.use(morgan())
 let persons = [{
     "name": "Arto Hellas",
     "number": "040-123456",
