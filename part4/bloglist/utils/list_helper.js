ffc1b36b2f28dede17f21ac6c9474ae321cd415c
Mouli <venkatmouli823@gmail.com>
2021-06-10 14:50:47 +0530

    4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,16 @@
 const dummy = (blogs) => {
     return 1;
 }
+const totalLikes = (blogs) => {
+    let totallikes;
+    blogs.length === 0 ? 
+    totallikes = 0 
+    :
+    totallikes = blogs.reduce((accumulator,currentValue)=> accumulator + currentValue.likes,0)
+    return totallikes
+}
 
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
