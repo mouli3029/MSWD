d30132cceb372897b32cf8a342ee98305ae4e25f
Mouli <venkatmouli823@gmail.com>
2021-06-14 12:03:12 +0530

    4.10

--- part4/bloglist/tests/api_helper.test.js
+++ part4/bloglist/tests/api_helper.test.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const helper = require('./blog_helper');
-const Blog = require('../models/blog')
+const Blog = require('../models/blog');
+const blogRouter = require('../controllers/blog');
 const api = supertest(app)
 
 beforeEach(async () => {
     await Blog.deleteMany({})
-
-    let blogObject = new Blog(helper.initialBlogs[0])
+    /* let blogObject = new Blog(helper.initialBlogs[0])
     await blogObject.save()
     blogObject = new Blog(helper.initialBlogs[1])
     await blogObject.save()
@@ -20,7 +20,11 @@ beforeEach(async () => {
     blogObject = new Blog(helper.initialBlogs[4])
     await blogObject.save()
     blogObject = new Blog(helper.initialBlogs[5])
-    await blogObject.save()
+    await blogObject.save() */
+
+    const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
+    const promiseArry = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArry)
 
 }, 60000)
 
@@ -36,6 +40,24 @@ describe('HTTP GET TESTING', () => {
     })
 })
 
+describe('HTTP POST Testing', () => {
+    test('check new blog is added and count of total no of blogs increased by one', async () => {
+        const newBLog = {
+            title: "React Redux",
+            author: "Mouli",
+            url: "https://reactredux.com/",
+            likes: 5
+        }
+        await api.post('/api/blogs')
+            .send(newBLog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const response = await api.get('/api/blogs')
+        const titles = response.body.map(b => b.title)
+        expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+    })
+})
 
 afterAll(() => {
     mongoose.connection.close()
